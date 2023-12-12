ec7d22ed3780af89e56b81866453202597701adb
hisbanshiraz <hisbanbardai@gmail.com>
2023-12-12 16:21:51 -0700

    Setup addEventListener function for start, stop and reset buttons and their event handler functions

--- script.js
+++ script.js
@@ -2,3 +2,17 @@ const timerEle = document.getElementById("timer");
 const startEle = document.getElementById("start");
 const stopEle = document.getElementById("stop");
 const resetEle = document.getElementById("reset");
+let startTime = 0;
+let elapsedTime = 0;
+
+function startTimer() { }
+
+function stopTimer() { }
+
+function resetTimer() {}
+
+startEle.addEventListener("click", function() {});
+
+stopEle.addEventListener("click", function () {});
+
+resetEle.addEventListener("click", function () {});

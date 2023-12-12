28b1db23795ecd23db31993268141e5a3daaa883
hisbanshiraz <hisbanbardai@gmail.com>
2023-12-12 16:09:19 -0700

    Setup script.js file and add all the needed elements

---
+++ script.js
@@ -0,0 +1,4 @@
+const timerEle = document.getElementById("timer");
+const startEle = document.getElementById("start");
+const stopEle = document.getElementById("stop");
+const resetEle = document.getElementById("reset");
